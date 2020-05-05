0242473276379315a15ffe1dd4f4dcb36218b1ca
zahidurrehman <zahid.urrehman@gtsone.com>
2020-05-05 22:17:42 +0500

    Error Handling, body-parser and handling CORS error

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const body = require('body-parser');
 const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 //morgan used for logging purpose
 app.use(morgan('dev'));
+app.use(body.urlencoded({ extended: false }));
+app.use(body.json({}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH');
+        return res.status(200).json({});
+    }
+    next();
+});
 //routes which will handle incoming requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
